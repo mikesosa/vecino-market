14ea7a5c1e1e910abd7ade6f7379920855d85461
mikesosa <michael@buydepa.com>
2022-12-30 02:10:20 -0500

    improvements

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,10 +1,15 @@
 import React, { createContext, useState } from "react";
+import { useEffect } from "react";
 
 export const ThemeContext = createContext("");
 
 export default function ThemeContextProvider(props) {
   const [isDarkMode, setIsDarkMode] = useState(true);
 
+  useEffect(() => {
+    const classes = Object.values(document.documentElement.classList);
+    if (classes.includes("dark")) setIsDarkMode(true);
+  }, []);
   return (
     <ThemeContext.Provider
       value={{
